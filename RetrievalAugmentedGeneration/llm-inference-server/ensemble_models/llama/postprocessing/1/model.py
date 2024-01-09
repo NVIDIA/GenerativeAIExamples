ac4194ac16488f17c40e4f2cd513cad4f811c4ed
y-h-Lin <d03945002@ntu.edu.tw>
2024-01-09 19:03:10 +0800

    [postprocessing] Fix the issue of character display
    Process multiple tokens to allow other languages to display normally, including Chinese, Japanese, and Korean.

--- RetrievalAugmentedGeneration/llm-inference-server/ensemble_models/llama/postprocessing/1/model.py
+++ RetrievalAugmentedGeneration/llm-inference-server/ensemble_models/llama/postprocessing/1/model.py
@@ -16,7 +16,7 @@
 
 import json
 import os
-
+import logging
 import numpy as np
 import triton_python_backend_utils as pb_utils
 from transformers import LlamaTokenizer
@@ -32,7 +32,7 @@ class TritonPythonModel:
     """Your Python model must use the same class name. Every Python model
     that is created must have "TritonPythonModel" as the class name.
     """
-
+    
     def initialize(self, args):
         """`initialize` is called only once when the model is being loaded.
         Implementing `initialize` function is optional. This function allows
@@ -48,6 +48,7 @@ class TritonPythonModel:
           * model_version: Model version
           * model_name: Model name
         """
+        self.cumList = []
         # Parse model configs
         self.model_config = model_config = json.loads(args["model_config"])
 
@@ -83,7 +84,6 @@ class TritonPythonModel:
         """
 
         responses = []
-
         # Every Python backend must iterate over everyone of the requests
         # and create a pb_utils.InferenceResponse for each of them.
         for request in requests:
@@ -138,11 +138,41 @@ class TritonPythonModel:
 
         # handle typical tokens
         tokens = self.tokenizer.convert_ids_to_tokens(token_id)
+
+        if len(tokens) == 6 and tokens[0] == "<":
+            self.cumList.append(tokens)
+            if len(self.cumList) == 3:
+                hex_txt = "".join(self.cumList)
+                self.cumList = []
+                hex_txt = bytes.fromhex(hex_txt.replace("<0x","").replace(">","")).decode("utf-8")
+                return f"{hex_txt}"
+            return ""
+            
         if ord(tokens[0]) == SPACE_CHAR:
+            self.cumList = []
             return f" {tokens[1:]}"
+
         if ord(tokens[0]) == NEWLINE_CHAR:
+            self.cumList = []
             return "\n"
+
+        self.cumList = []
         return tokens
+    
+
+    def _postprocessing_stream_full(self, tokens_batch, outputs):
+        outputs_decoded = []
+        tokens_batch = tokens_batch.tolist()
+        token_ids = tokens_batch[0][0]
+        outputs.extend(token_ids)
+        self._decode(outputs, outputs_decoded)
+        return outputs_decoded
+
+    def _decode(self, outputs, outputs_decoded):
+        decoded = self.tokenizer.decode(outputs)
+        decoded = decoded.replace("�", "").strip()
+        outputs_decoded.append(decoded)
+
 
     def _postprocessing(self, tokens_batch):
         tokens_batch = tokens_batch.tolist()
@@ -152,7 +182,7 @@ class TritonPythonModel:
             for token_ids in beam_tokens
             for token_id in token_ids
         ]
-
+#            for token_id in token_ids
         # for beam_tokens in tokens_batch:
         #     for token_ids in beam_tokens:
         #         for token_id in token_ids:
