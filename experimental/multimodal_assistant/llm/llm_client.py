c81cdc62a9eb61fcb7b5249631ac78e1164f002e
Shubhadeep Das <149712532+shubhadeepd@users.noreply.github.com>
2024-02-01 01:44:23 +0530

    experimental: add multi-modal end to end RAG example (#41)

---
+++ experimental/multimodal_assistant/llm/llm_client.py
@@ -0,0 +1,38 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023-2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from llm.llm import create_llm
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.messages import HumanMessage
+
+class LLMClient:
+    def __init__(self, model_name="mixtral_8x7b", model_type="NVIDIA"):
+        self.llm = create_llm(model_name, model_type)
+
+    def chat_with_prompt(self, system_prompt, prompt):
+        langchain_prompt = ChatPromptTemplate.from_messages([("system", system_prompt), ("user", "{input}")])
+        chain = langchain_prompt | self.llm | StrOutputParser()
+        response = chain.stream({"input": prompt})
+
+        return response
+
+    def multimodal_invoke(self, b64_string, steer=False, creativity=0, quality=9, complexity=0, verbosity=8):
+        message = HumanMessage(content=[{"type": "text", "text": "Describe this image in detail:"},
+                                        {"type": "image_url", "image_url": {"url": f"data:image/png;base64,{b64_string}"},}])
+        if steer:
+            return self.llm.invoke([message], labels={"creativity": creativity, "quality": quality, "complexity": complexity, "verbosity": verbosity})
+        else:
+            return self.llm.invoke([message])
