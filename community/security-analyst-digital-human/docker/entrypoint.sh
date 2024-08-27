97c344e61c449ca01ba5b3d9380edcb715bfc426
Katherine Huang <katherineh@nvidia.com>
2024-08-26 23:40:30 -0700

    added as normal folder instead of repo

--- community/security-analyst-digital-human/docker/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Activate "morpheus" conda environment
-. /opt/conda/etc/profile.d/conda.sh
-conda activate morpheus
-
-# Run whatever user wants
-exec "$@"
--- community/security-analyst-digital-human/docker/start_jupyter.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-nohup jupyter-lab --allow-root --ip=0.0.0.0 --port=8888 --no-browser --NotebookApp.token='' > /dev/null 2>&1 &
