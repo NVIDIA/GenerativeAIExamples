35a04a97e312178f2b56d0c5e2d0b7927c39a30b
Jay Rodge <jrodge@nvidia.com>
2024-08-29 08:52:55 -0700

    added copyright headers

--- community/multimodal-rag/app.py
+++ community/multimodal-rag/app.py
@@ -1,3 +1,18 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023-2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 import streamlit as st
 from llama_index.core import Settings
@@ -26,7 +41,7 @@ def create_index(documents):
             port = 19530,
             dim = 1024
     )
-    # vector_store = MilvusVectorStore(uri="./milvus_demo.db", dim=1024, overwrite=True)
+    # vector_store = MilvusVectorStore(uri="./milvus_demo.db", dim=1024, overwrite=True) #For CPU only vector store
     storage_context = StorageContext.from_defaults(vector_store=vector_store)
     return VectorStoreIndex.from_documents(documents, storage_context=storage_context)
 
