2177f271f566d8d371863cdb9fa7b2509ae06f1d
scharugulla <scharugulla@nvidia.com>
2025-01-30 11:45:15 +0530

    updated with the changes

---
+++ community/bat_log_analysis/binary_score_models.py
@@ -0,0 +1,13 @@
+from langchain_core.pydantic_v1 import BaseModel,Field
+# Data models
+class GradeDocuments(BaseModel):
+    """Binary score for relevance check on retrieved documents."""
+    binary_score: str = Field(description="Documents are relevant to the question, 'yes' or 'no'")
+
+class GradeHallucinations(BaseModel):
+    """Binary score for hallucination present in generation answer."""
+    binary_score: str = Field(description="Answer is grounded in the facts, 'yes' or 'no'")
+
+class GradeAnswer(BaseModel):
+    """Binary score to assess answer addresses question."""
+    binary_score: str = Field(description="Answer addresses the question, 'yes' or 'no'")
