53244428fd7962525b26aa07f8b5d0f8ca45ca87
Vineeth Kalluru <vikalluru@nvidia.com>
2025-07-31 20:13:18 -0700

    Phoenix and Catalyst Tracing works. Implemented Eval harness successfully
    Signed-off-by: Vineeth Kalluru <vikalluru@nvidia.com>

--- industries/manufacturing/predictive_maintenance_agent/src/predictive_maintenance_agent/register.py
+++ industries/manufacturing/predictive_maintenance_agent/src/predictive_maintenance_agent/register.py
@@ -8,10 +8,5 @@ from . import plot_distribution_tool
 from . import plot_comparison_tool
 from . import plot_line_chart_tool
 from . import code_generation_assistant
-# from . import plot_sensor_over_RUL_tool
-# from . import load_models_and_predict_RUL_tool
-# from . import plot_anomaly_tool
-# from . import distribution_histogram_rul_tool
-# from . import plot_prediction_errors_tool
-# from . import plot_rul_error_by_health_state_tool
-# from . import generate_chart_tool
+from . import llm_judge_evaluator_register
+from . import custom_trajectory_evaluator_register
