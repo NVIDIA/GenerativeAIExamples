4d120262bdb39ac286c7d7855f8e8e4fbff0480b
Alexander <157773158+bvonodiripsa@users.noreply.github.com>
2025-10-16 15:09:50 -0700

    Delete community/SQLServer_AI_with_NVIDIA_NIM/scripts directory

--- community/SQLServer_AI_with_NVIDIA_NIM/scripts/4_ChangeExternalModel.sql
+++
@@ -1,8 +0,0 @@
-EXEC dbo.CreateExternalModel
-    --@Location = 'https://ngc-test.ashysmoke-ef652ce7.westus.azurecontainerapps.io/v1/embeddings',
-    @Location = 'https://192.168.10.218:8000/v1/embeddings',
-    @Model = 'nvidia/nv-embedqa-e5-v5-query',
-    @Name = 'EmbedE5_OpenAI';
-
-
-SELECT * FROM sys.external_models WHERE name = 'EmbedE5_OpenAI';
