65901d9e07e007bd12f15ade18f0325b77183d79
Alexander <157773158+bvonodiripsa@users.noreply.github.com>
2025-10-17 13:00:32 -0700

    Refactor nvidia_run_proc_embedding procedure

--- community/SQLServer_AI_with_NVIDIA_NIM/scripts/2_proc_embedding.sql
+++ community/SQLServer_AI_with_NVIDIA_NIM/scripts/2_proc_embedding.sql
@@ -1,4 +1,20 @@
-﻿CREATE OR ALTER PROCEDURE dbo.nvidia_run_proc_embedding
+/*
+ Copyright (c) 2025, NVIDIA CORPORATION.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+CREATE OR ALTER PROCEDURE dbo.nvidia_run_proc_embedding
     @ModelName         NVARCHAR(255),
     @VectorSize        INT,
     @VectorColumnName  SYSNAME,      -- e.g., N'Embed_E5_ACA'
@@ -120,3 +136,4 @@ BEGIN
     CLOSE cur; DEALLOCATE cur;
 END
 GO
+
