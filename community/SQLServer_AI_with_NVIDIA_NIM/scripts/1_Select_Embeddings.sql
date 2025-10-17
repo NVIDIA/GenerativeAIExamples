4af8f26f245d358356e0f3557873ca4c96834ba9
Alexander <157773158+bvonodiripsa@users.noreply.github.com>
2025-10-17 12:59:35 -0700

    Refactor SQL script for selecting embeddings

--- community/SQLServer_AI_with_NVIDIA_NIM/scripts/1_Select_Embeddings.sql
+++ community/SQLServer_AI_with_NVIDIA_NIM/scripts/1_Select_Embeddings.sql
@@ -1,3 +1,19 @@
+/*
+ Copyright (c) 2025, NVIDIA CORPORATION.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 -- Output columns in this exact order:
 -- ProductID, ProductDescriptionID, ProductModelID, CultureID, Embedding, Embed_E5_ACA, Description, ModifiedDate
 -- If Embed_E5_ACA doesn't exist yet, return NULL AS [Embed_E5_ACA].
@@ -45,6 +61,7 @@ WHERE rn = 1
 ORDER BY RE.ProductDescriptionID;';
 
 EXEC sp_executesql @sql;
+
 
 
 
